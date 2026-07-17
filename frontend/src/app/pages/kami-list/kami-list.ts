17560420d520f0fdb14fc09b084ad0cc75bafb43
zineb-kourichel <zinebkourichel@gmail.com>
2026-07-18 00:08:45 +0100

    Redesign Kami list page with Japanese-inspired styling (ema plaques, hanko seals)

--- frontend/src/app/pages/kami-list/kami-list.ts
+++ frontend/src/app/pages/kami-list/kami-list.ts
@@ -1,8 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Kami } from '../../services/kami';
 import { KamiData } from '../../models/kami.model';
+
+const KANJI_MAP: Record<string, string> = {
+  'Amaterasu': '天照',
+  'Susanoo': '須佐之男',
+  'Tsukuyomi': '月読',
+  'Izanagi': '伊邪那岐',
+  'Izanami': '伊邪那美',
+  'Inari': '稲荷',
+  'Hachiman': '八幡',
+  'Raijin': '雷神',
+  'Fujin': '風神',
+  'Ebisu': '恵比寿'
+};
 
 @Component({
   selector: 'app-kami-list',
@@ -11,26 +24,27 @@ import { KamiData } from '../../models/kami.model';
   styleUrl: './kami-list.css',
 })
 export class KamiList implements OnInit {
-  kamiList: KamiData[] = [];
-  loading = true;
-  error = '';
+  kamiList = signal<KamiData[]>([]);
+  loading = signal(true);
+  error = signal('');
 
   constructor(private kamiService: Kami) {}
 
   ngOnInit(): void {
-    console.log('KamiList ngOnInit running');
     this.kamiService.getAllKami().subscribe({
       next: (data) => {
-        console.log('Data received:', data);
-        this.kamiList = data;
-        this.loading = false;
+        this.kamiList.set(data);
+        this.loading.set(false);
       },
       error: (err) => {
-        console.log('Error occurred:', err);
-        this.error = 'Could not load Kami. Is the backend running?';
-        this.loading = false;
+        this.error.set('Could not load Kami. Is the backend running?');
+        this.loading.set(false);
         console.error(err);
       }
     });
+  }
+
+  kanjiFor(name: string): string {
+    return KANJI_MAP[name] ?? '神';
   }
 }
